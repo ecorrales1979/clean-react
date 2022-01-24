43ad5fc389ce53053749d7523641e063cac04fdf
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-01-24 00:31:01 -0300

    chore: add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'avoid',
+  arrowParens: 'avoid',
+  htmlWhitespaceSensitivity: 'css',
+}
