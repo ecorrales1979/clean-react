47b70b013c12dc71a84c9709f6b7c9c7b7dfe2b7
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-01-24 23:36:16 -0300

    style: fix spaces in HttpClient mock file

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,9 +1,9 @@
 import { HttpPostClient } from 'data/protocols/http/http-post-client'
 
 export class HttpPostClientSpy implements HttpPostClient {
-      url?: string
+  url?: string
 
-      async post (url: string): Promise<void> {
-        this.url = url
-      }
+  async post (url: string): Promise<void> {
+    this.url = url
+  }
 }
