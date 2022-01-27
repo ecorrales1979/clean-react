a88377ef31273584ceb3275fcac5d0dea7377da4
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-01-27 14:00:26 -0300

    test: ensure AxiosHttpClient calls axios with correct url

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,16 @@
+import faker from '@faker-js/faker'
+import axios from 'axios'
+
+import { AxiosHttpClient } from './axios-http-client'
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+describe('AxiosHttpClient', () => {
+  it('should call Axios with correct url', async () => {
+    const url = faker.internet.url()
+    const sut = new AxiosHttpClient()
+    await sut.post({ url })
+    expect(mockedAxios).toHaveBeenCalledWith(url)
+  })
+})
