d7df2a6b8c492ef5981d1762de9071b9c668f60e
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-01-27 14:09:11 -0300

    test: ensure AxiosHttpClient calls axios with correct verb

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -12,10 +12,10 @@ const makeSut = (): { sut: AxiosHttpClient } => {
 }
 
 describe('AxiosHttpClient', () => {
-  it('should call Axios with correct url', async () => {
+  it('should call Axios with correct url and verb', async () => {
     const url = faker.internet.url()
     const { sut } = makeSut()
     await sut.post({ url })
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
   })
 })
