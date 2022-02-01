7f6786c74368f78b0e19e293a89f95cc2d9f9014
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-01 07:07:46 -0300

    test: ensure login fields start with correct state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -10,5 +10,11 @@ describe('Login page', () => {
     expect(loadingWrap.childElementCount).toBe(0)
     const submitBtn = getByTestId('submit') as HTMLButtonElement
     expect(submitBtn.disabled).toBe(true)
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('🔴')
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('🔴')
   })
 })
