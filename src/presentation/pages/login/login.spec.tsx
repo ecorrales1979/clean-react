c6fafa6966940db1b4b1e203dcc35475374b1f43
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-01 06:28:25 -0300

    refactor: rename first login test

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react'
 import Login from './login'
 
 describe('Login page', () => {
-  it('Should not render spinner and error on start', () => {
+  it('Should start with initial state', () => {
     const { getByTestId } = render(<Login />)
     const loadingWrap = getByTestId('loading-wrap')
     expect(loadingWrap.childElementCount).toBe(0)
