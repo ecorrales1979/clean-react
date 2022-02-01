6202afb1c01a8ccf71061d50ab05f1fdfd8bc54c
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-01 19:35:59 -0300

    refactor: move SUT criation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,19 +1,31 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 
 import Login from './login'
 
+interface SutTypes {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+
+  return {
+    sut
+  }
+}
+
 describe('Login page', () => {
   it('Should start with initial state', () => {
-    const { getByTestId } = render(<Login />)
-    const loadingWrap = getByTestId('loading-wrap')
+    const { sut } = makeSut()
+    const loadingWrap = sut.getByTestId('loading-wrap')
     expect(loadingWrap.childElementCount).toBe(0)
-    const submitBtn = getByTestId('submit') as HTMLButtonElement
+    const submitBtn = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitBtn.disabled).toBe(true)
-    const emailStatus = getByTestId('email-status')
+    const emailStatus = sut.getByTestId('email-status')
     expect(emailStatus.title).toBe('Campo obrigatório')
     expect(emailStatus.textContent).toBe('🔴')
-    const passwordStatus = getByTestId('password-status')
+    const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(passwordStatus.textContent).toBe('🔴')
   })
