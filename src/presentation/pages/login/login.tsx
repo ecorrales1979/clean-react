76aa814098b6731731b1cf8113e08552f4852ab5
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-02 10:33:25 -0300

    feat: ensure login calls validation with correct password

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -18,6 +18,7 @@ const Login: React.FC<Props> = ({ validation }) => {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
+    password: '',
     emailError: 'Campo obrigatório',
     passwordError: 'Campo obrigatório',
     mainError: ''
@@ -26,6 +27,10 @@ const Login: React.FC<Props> = ({ validation }) => {
   useEffect(() => {
     validation.validate({ email: state.email })
   }, [state.email])
+
+  useEffect(() => {
+    validation.validate({ password: state.password })
+  }, [state.password])
 
   return (
     <div className="login">
