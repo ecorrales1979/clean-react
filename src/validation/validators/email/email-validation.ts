0487f27957433eedadc79e7af623cab3a28014f2
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-17 00:04:03 -0300

    feat:  ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -5,6 +5,8 @@ export class EmailValidation implements FieldValidation {
   constructor (readonly field: string) {}
 
   validate (value: string): Error | null {
-    return new InvalidFieldError(this.field)
+    const emailRegex =
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    return emailRegex.test(value) ? null : new InvalidFieldError(this.field)
   }
 }
