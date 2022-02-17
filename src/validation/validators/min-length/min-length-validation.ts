f97e86985b3f242d4f4f940755e740e3af37b944
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-17 20:47:17 -0300

    feat: ensure MinLengthValidation allows empty value

--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -4,6 +4,8 @@ import { FieldValidation } from '@/validation/protocols/field-validation'
 export class MinLengthValidation implements FieldValidation {
   constructor (readonly field: string, private readonly minLength: number) {}
   validate (value: string): Error | null {
+    if (!value) return null
+
     return value.length < this.minLength
       ? new InvalidFieldError(this.field)
       : null
