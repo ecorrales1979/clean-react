57d50fb878098b669f6c1b473caf165cad590b42
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-17 20:45:07 -0300

    feat: ensure EmailValidation allows empty value

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -5,6 +5,8 @@ export class EmailValidation implements FieldValidation {
   constructor (readonly field: string) {}
 
   validate (value: string): Error | null {
+    if (!value) return null
+
     const emailRegex =
       /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     return emailRegex.test(value) ? null : new InvalidFieldError(this.field)
