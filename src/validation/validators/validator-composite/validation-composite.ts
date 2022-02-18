0c797c3c0d9ce344ebb3d7b0725bcc0428248df0
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-17 23:33:58 -0300

    feat: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/validator-composite/validation-composite.ts
@@ -0,0 +1,15 @@
+import { Validation } from '@/presentation/protocols/validation'
+import { FieldValidation } from '@/validation/protocols/field-validation'
+
+export class ValidationComposite implements Validation {
+  constructor (private readonly validators: FieldValidation[]) {}
+  validate (fieldName: string, fieldValue: string): string | null {
+    const validators = this.validators.filter(v => v.field === fieldName)
+    for (const validator of validators) {
+      const error = validator.validate(fieldValue)
+      console.log('error', error?.message)
+      if (error) return error.message
+    }
+    return null
+  }
+}
