59717adad678702a9c4742adf75e261f20e21c5b
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-21 20:35:58 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,5 @@
+export * from './builder/validation-builder'
+export * from './email/email-validation'
+export * from './min-length/min-length-validation'
+export * from './required-field/required-field-validation'
+export * from './validator-composite/validation-composite'
