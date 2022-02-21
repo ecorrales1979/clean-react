89c00bb9925a7b62157077796a5586f109529177
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-21 20:55:48 -0300

    test: ensure ValidationBuilder returns a list of validations

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -20,4 +20,13 @@ describe('ValidationBuilder', () => {
     const validations = sut.field('any_field').min(5).build()
     expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
   })
+
+  it('Should return a list of validations', () => {
+    const validations = sut.field('any_field').required().min(5).email().build()
+    expect(validations).toEqual([
+      new RequiredFieldValidation('any_field'),
+      new MinLengthValidation('any_field', 5),
+      new EmailValidation('any_field')
+    ])
+  })
 })
