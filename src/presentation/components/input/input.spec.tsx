6bb59a65c3185d03795d50a92b6b8e571be629a3
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-22 08:51:46 -0300

    refactor: use faker on Input components tests

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import { fireEvent, render, RenderResult } from '@testing-library/react'
+import faker from '@faker-js/faker'
 
 import { Input } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 
-const makeSut = (): RenderResult => {
+const makeSut = (fieldName: string): RenderResult => {
   return render(
     <Context.Provider value={{ state: {}, setState: jest.fn() }}>
-      <Input name="field" />
+      <Input name={fieldName} />
     </Context.Provider>
   )
 }
 
 describe('Input Component', () => {
   it('Should begin with readonly', () => {
-    const sut = makeSut()
-    const input = sut.getByTestId('field') as HTMLInputElement
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const input = sut.getByTestId(field) as HTMLInputElement
     expect(input.readOnly).toBe(true)
   })
 
   it('Should remove readonly on focus', () => {
-    const sut = makeSut()
-    const input = sut.getByTestId('field') as HTMLInputElement
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const input = sut.getByTestId(field) as HTMLInputElement
     fireEvent.focus(input)
     expect(input.readOnly).toBe(false)
   })
