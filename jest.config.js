2ef0796f03dccbbdf0216844b03b8c630fc764f6
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-22 09:00:15 -0300

    chore: remove export redefinition files from jest coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
     '!<rootDir>/src/presentation/components/router/**/*',
     '!**/*.d.ts'
   ],
