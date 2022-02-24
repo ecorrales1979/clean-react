11c846ef1cebcd188c152682ff6e3c1e6b3bd01d
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-02-23 21:48:21 -0300

    refactor: rename SetStorageSpy to SetStorageMock

--- src/data/mocks/mock-storage.ts
+++ src/data/mocks/mock-storage.ts
@@ -1,6 +1,6 @@
 import { SetStorage } from '@/data/protocols/cache/set-storage'
 
-export class SetStorageSpy implements SetStorage {
+export class SetStorageMock implements SetStorage {
   public key?: string
   public value: any
 
