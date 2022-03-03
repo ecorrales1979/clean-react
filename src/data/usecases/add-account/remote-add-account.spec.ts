becd69125f9dbd1d09383677f7f4a64fd6911a42
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-02 22:25:43 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 500

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -54,4 +54,13 @@ describe('', () => {
     const promise = sut.add(mockAddAccountParams())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  it('should throw UnexpectedError if HttpPostClient returns 500', async () => {
+    const { httpPostClientSpy, sut } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
