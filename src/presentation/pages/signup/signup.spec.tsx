373761f660960fab996c0784dc4d72531d9cbdde
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-03 22:34:36 -0300

    test: ensure SignUp changes passwordConfirmation to valid status if validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -84,4 +84,10 @@ describe('SignUp page', () => {
     Helper.populateField(sut, 'password')
     Helper.testStatusForField(sut, 'password')
   })
+
+  it('Should show valid passwordConfirmation state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'passwordConfirmation')
+    Helper.testStatusForField(sut, 'passwordConfirmation')
+  })
 })
