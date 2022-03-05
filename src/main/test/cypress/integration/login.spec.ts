266b33a81ea6743ccb7aa925b9d1b252ee40144d
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-05 15:01:46 -0300

    test: ensure Login presents error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,3 +1,5 @@
+import faker from '@faker-js/faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('/login')
@@ -10,6 +12,19 @@ describe('Login', () => {
       .should('contain.text', '🔴')
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('be.disabled')
+    cy.getByTestId('loading-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'email is invalid')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'password is invalid')
       .should('contain.text', '🔴')
     cy.getByTestId('submit').should('be.disabled')
     cy.getByTestId('loading-wrap').should('not.have.descendants')
