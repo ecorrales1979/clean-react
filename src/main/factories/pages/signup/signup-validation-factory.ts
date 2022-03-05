fbb467128c775b6b18a5a085ad2c4ae92421427e
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-05 02:35:36 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,17 @@
+import {
+  ValidationBuilder as Builder,
+  ValidationComposite
+} from '@/validation/validators'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  return ValidationComposite.build([
+    ...Builder.field('name').required().min(2).build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').required().min(5).build(),
+    ...Builder.field('passwordConfirmation')
+      .required()
+      .min(5)
+      .sameAs('password')
+      .build()
+  ])
+}
