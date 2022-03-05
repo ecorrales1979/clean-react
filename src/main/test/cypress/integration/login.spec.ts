66e23dfa1f9eb3e27f7ee2e5bb604ee7c4cd0edd
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-05 14:42:17 -0300

    test: ensure Login loads with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,17 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('/login')
+  })
+  it('Should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readonly')
+    cy.getByTestId('password').should('have.attr', 'readonly')
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('be.disabled')
+    cy.getByTestId('loading-wrap').should('not.have.descendants')
   })
 })
