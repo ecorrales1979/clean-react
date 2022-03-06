673432df3db5f45eb1c7222b1ffc0c193e02e869
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-06 13:18:22 -0300

    refactor: use faker to substitute login data on success tests

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -85,8 +85,8 @@ describe('Login', () => {
       body: { invalidProperty: faker.datatype.uuid() },
       delay: 50
     })
-    cy.getByTestId('email').focus().type('mango@gmail.com')
-    cy.getByTestId('password').focus().type('12345')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
     cy.getByTestId('submit').click()
     cy.getByTestId('spinner').should('exist')
     cy.getByTestId('main-error').should('not.exist')
@@ -102,8 +102,8 @@ describe('Login', () => {
       body: { accessToken: faker.datatype.uuid() },
       delay: 50
     })
-    cy.getByTestId('email').focus().type('mango@gmail.com')
-    cy.getByTestId('password').focus().type('12345')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
     cy.getByTestId('submit').click()
     cy.getByTestId('spinner').should('exist')
     cy.getByTestId('main-error').should('not.exist')
