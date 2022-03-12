8ca6c6fbe534bef0e8f49bdd487c1e9d563abebb
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-12 00:00:13 -0300

    refactor: move icon to its own component

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import './survey-list-styles.scss'
-import { Footer, Header } from '@/presentation/components'
+import { Footer, Header, Icon } from '@/presentation/components'
 
 const SurveyList: React.FC = () => {
   return (
@@ -12,9 +12,7 @@ const SurveyList: React.FC = () => {
         <ul>
           <li>
             <div className="surveyContent">
-              <div className={['iconWrap', 'red'].join(' ')}>
-                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA0klEQVQ4EWNgIAH8//+/AYhLSNCCWynUMCD1/zcQG+BWSYQMkmEgA0Egjght2JUANYO8iQ4MsasmIAo0BZthP4DirAS0YkrjMAzk0tOYqgmIADUVgnTiADPxakfStAWmECj2DkmcWOYjoEJPRpBqmEGMQABiI4vB5IikH1PbQAYmIm0mVtlLahu4nJpe/gf0hho1XbgVGKd3qWngRFBA4/LyX6AcKZZdBbpOB2QgLk1nQJIkgElwtaBEDAXIOUULKHYSiP/CJHHQX4Hic4CYBWYgADx8PyqFiuhJAAAAAElFTkSuQmCC" />
-              </div>
+              <Icon iconName="thumbUp" className="iconWrap" />
               <time>
                 <span className="day">11</span>
                 <span className="month">03</span>
