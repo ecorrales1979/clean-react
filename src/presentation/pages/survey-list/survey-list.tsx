431630265b277c6397168080cb8dd0e506a4e01c
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-11 22:52:07 -0300

    feat: add survey icon to show status

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -20,28 +20,9 @@ const SurveyList: React.FC = () => {
         <ul>
           <li>
             <div className="surveyContent">
-              <time>
-                <span className="day">11</span>
-                <span className="month">03</span>
-                <span className="year">2022</span>
-              </time>
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className="surveyContent">
-              <time>
-                <span className="day">11</span>
-                <span className="month">03</span>
-                <span className="year">2022</span>
-              </time>
-              <p>Qual é seu framework web favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className="surveyContent">
+              <div className={['iconWrap', 'red'].join(' ')}>
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA0klEQVQ4EWNgIAH8//+/AYhLSNCCWynUMCD1/zcQG+BWSYQMkmEgA0Egjght2JUANYO8iQ4MsasmIAo0BZthP4DirAS0YkrjMAzk0tOYqgmIADUVgnTiADPxakfStAWmECj2DkmcWOYjoEJPRpBqmEGMQABiI4vB5IikH1PbQAYmIm0mVtlLahu4nJpe/gf0hho1XbgVGKd3qWngRFBA4/LyX6AcKZZdBbpOB2QgLk1nQJIkgElwtaBEDAXIOUULKHYSiP/CJHHQX4Hic4CYBWYgADx8PyqFiuhJAAAAAElFTkSuQmCC" />
+              </div>
               <time>
                 <span className="day">11</span>
                 <span className="month">03</span>
