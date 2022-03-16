c0a2d94e6505de15fe317d35a5bbfa44b7b71fb1
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-15 21:29:47 -0300

    test: ensure LocalStorageAdapter calls localStorage.getItem with correct value

--- src/data/mocks/mock-cache.ts
+++ src/data/mocks/mock-cache.ts
@@ -1,4 +1,4 @@
-import { SetStorage } from '@/data/protocols/cache/set-storage'
+import { SetStorage } from '@/data/protocols/cache'
 
 export class SetStorageMock implements SetStorage {
   public key?: string
--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -10,11 +10,21 @@ describe('LocalStorageAdapter', () => {
     localStorage.clear()
   })
 
-  it('Should call localStorage with correct values', () => {
+  it('Should call localStorage.setItem with correct values', () => {
     const sut = makeSut()
     const key = faker.database.column()
     const value = JSON.parse(faker.datatype.json())
     sut.set(key, value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
+  })
+
+  it('Should call localStorage.getItem with correct value', () => {
+    const sut = makeSut()
+    const key = faker.database.column()
+    const value = faker.datatype.json()
+    jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(value)
+    const objString = sut.get(key)
+    expect(objString).toEqual(JSON.parse(value))
+    expect(localStorage.getItem).toHaveBeenCalledWith(key)
   })
 })
