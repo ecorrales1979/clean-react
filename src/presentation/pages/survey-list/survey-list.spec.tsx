ae4d9bb64f09f0cc33957bd75f8ed22da66dcd1f
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-19 19:50:09 -0300

    test: ensure SurveyList calls loadSurveyList

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 
+import { SurveyModel } from '@/domain/models'
+import { LoadSurveyList } from '@/domain/usecases'
 import { SurveyList } from '@/presentation/pages'
 
-const makeSut = (): void => {
-  render(<SurveyList />)
+class LoadSurveyListSpy implements LoadSurveyList {
+  async loadAll (): Promise<SurveyModel[]> {
+    return []
+  }
+}
+
+interface SutTypes {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+  jest.spyOn(loadSurveyListSpy, 'loadAll')
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+  return { loadSurveyListSpy }
 }
 
 describe('SurveyList page', () => {
@@ -12,5 +27,10 @@ describe('SurveyList page', () => {
     makeSut()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+  })
+
+  it('Should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut()
+    expect(loadSurveyListSpy.loadAll).toHaveBeenCalledTimes(1)
   })
 })
