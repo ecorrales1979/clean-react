14b9162a2ed2217a7a675e1c12d63b1b70d4eb70
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-19 23:00:18 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -2,15 +2,27 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 
 import { mockSurveyModel } from '@/domain/mocks'
+import { SurveyModel } from '@/domain/models'
 import { IconNameEnum } from '@/presentation/components'
 import { SurveyItem } from '@/presentation/pages/survey-list/components'
 
+interface SutTypes {
+  survey: SurveyModel
+}
+
+const makeSut = (data: Partial<SurveyModel>): SutTypes => {
+  const survey = mockSurveyModel()
+  Object.assign(survey, data)
+  render(<SurveyItem survey={survey} />)
+  return { survey }
+}
+
 describe('SurveyList component', () => {
   it('Should render with correct values', () => {
-    const survey = mockSurveyModel()
-    survey.didAnswer = true
-    survey.date = new Date('2020-01-10T00:00:00')
-    render(<SurveyItem survey={survey} />)
+    const { survey } = makeSut({
+      didAnswer: true,
+      date: new Date('2020-01-10T00:00:00')
+    })
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconNameEnum.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
     expect(screen.getByTestId('day')).toHaveTextContent('10')
