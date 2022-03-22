c7b7c3c02f059df23269e978d200a0abb5240c16
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-22 20:41:51 -0300

    refactor: move SUT creation to a factory method

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -1,10 +1,23 @@
 import { GetStorageSpy, mockGetRequest } from '@/data/mocks'
 import { AuthorizeHttpGetClientDecorator } from '@/main/decorators'
 
+interface SutType {
+  getStorageSpy: GetStorageSpy
+  sut: AuthorizeHttpGetClientDecorator
+}
+
+const makeSut = (): SutType => {
+  const getStorageSpy = new GetStorageSpy()
+  const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+  return {
+    getStorageSpy,
+    sut
+  }
+}
+
 describe('AuthorizeHttpGetClientDecorator', () => {
   it('Should call GetStorage with correct value', async () => {
-    const getStorageSpy = new GetStorageSpy()
-    const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+    const { getStorageSpy, sut } = makeSut()
     await sut.get(mockGetRequest())
     expect(getStorageSpy.key).toBe('account')
   })
