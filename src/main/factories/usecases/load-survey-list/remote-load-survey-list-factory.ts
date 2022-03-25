f7747df5cad785e7dbc6faa598a3930fe25470c7
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-24 22:04:50 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,7 +1,11 @@
 import { RemoteLoadSurveyList } from '@/data/usecases/load-survey-list/remote-load-survey-list'
 import { LoadSurveyList } from '@/domain/usecases'
-import { makeApiUrl, makeAxiosHttpClient } from '@/main/factories/http'
+import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators'
+import { makeApiUrl } from '@/main/factories/http'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAxiosHttpClient())
+  return new RemoteLoadSurveyList(
+    makeApiUrl('/surveys'),
+    makeAuthorizeHttpGetClientDecorator()
+  )
 }
