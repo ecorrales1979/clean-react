2651bd754aa9c7c7cf1f46431edc087db5c477d8
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2022-03-25 18:05:39 -0300

    refactor: remove unused code

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,9 +1,5 @@
-import faker from '@faker-js/faker'
-
 import { getCurrentAccountAdapter, setCurrentAccountAdapter } from './current-account-adapter'
-import { UnexpectedError } from '@/domain/errors'
 import { mockAccountModel } from '@/domain/mocks'
-import { AccountModel } from '@/domain/models'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 
 jest.mock('@/infra/cache/local-storage-adapter')
@@ -18,13 +14,6 @@ describe('CurrentAccountAdapter', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
-  })
-
-  it('Should throw UnexpectedError if an invalid account is provided', () => {
-    const account = { invalidProperty: faker.random.words() } as unknown as AccountModel
-    const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
-    expect(() => setCurrentAccountAdapter(account)).toThrowError(new UnexpectedError())
-    expect(setSpy).toHaveBeenCalledTimes(0)
   })
 
   it('Should call LocalStorageAdapter.get with correct value', () => {
